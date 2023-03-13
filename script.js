4f7ec0f0617dc767b1959eee154f11e09e5ee963
RodolfoEliasCruz <127742888+RodolfoEliasCruz@users.noreply.github.com>
2023-03-13 18:51:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//while
+
+var x = 0;
+while(x<60){
+    x=x+2; 
+    document.writeln(x);  
+}
+
+//do-while
+var y = 0;
+do{
+    y = y+3; 
+    document.writeln(y);   
+}
+while(y<60)
+
+//for
+var z = 0;
+for(z=5; z<51; z=z+5){
+    document.writeln(z);
+}
